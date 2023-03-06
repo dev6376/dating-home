514ca13ea809ae377ceb5b6f2ac348607efaddb7
dev6376 <112146127+dev6376@users.noreply.github.com>
2023-03-06 19:20:38 +0530

    Add files via upload

---
+++ js/volet.js
@@ -0,0 +1,44 @@
+var hamburgerMenu = document.querySelector(".menu-hamburger");
+var navbarLinks = document.querySelector(".links-navbar");
+
+function volet() {
+    navbarLinks.classList.toggle("mobile-menu");
+    hamburgerMenu.classList.toggle("open");
+}
+
+hamburgerMenu.addEventListener("click", volet);
+
+
+// swiper js------------>
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+});
+  
+
